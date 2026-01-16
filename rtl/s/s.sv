25c92ec60ae3badbd32a2580600fa7b69377c02a
Stephen Henry <stephenry@gmail.com>
2026-01-16 18:45:49 +0000

    Basic structure

---
+++ rtl/s/s.sv
@@ -0,0 +1,48 @@
+//========================================================================== //
+// Copyright (c) 2026, Stephen Henry
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//========================================================================== //
+
+`include "common_defs.vh"
+
+module s #(
+  // Vector width
+  parameter int W
+
+  // Encoded width
+, localparam int ENC_W = $clog2(W)
+) (
+  input wire logic [W - 1:0]                     x
+, input wire logic [ENC_W - 1:0]                 pos
+
+//
+, output wire logic [W - 1:0]                    y
+, output wire logic [EN_W - 1:0]                 y_enc
+);
+
+assign y = '0;
+assign y_enc = '0;
+
+endmodule : s
---
+++ src/syn/__main__.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from syn")
+
+    from tb.__main__ import main
+    main()
+
+if __name__ == "__main__":
+    main()
