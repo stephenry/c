442e5c3f6a3d506d11b5e2c3e5ea352eb937233e
Stephen Henry <stephenry@gmail.com>
2026-01-17 21:04:56 +0000

    foo

--- src/tb/__init__.py
+++ src/tb/__init__.py
@@ -1,4 +1,4 @@
-##========================================================================== ##
+## ========================================================================= ##
 ## Copyright (c) 2026, Stephen Henry
 ## All rights reserved.
 ##
@@ -23,4 +23,4 @@
 ## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ## POSSIBILITY OF SUCH DAMAGE.
-##========================================================================== ##
+## ========================================================================= ##
