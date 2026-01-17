5e8fbc901d7d1941287481406f0a60e39a59e355
Stephen Henry <stephenry@gmail.com>
2026-01-17 20:59:15 +0000

    foo

--- src/tb/__main__.py
+++ src/tb/__main__.py
@@ -1,14 +1,36 @@
 import cocotb
 
+
 def main():
     print("This is the main function.")
+
 
 @cocotb.test()
 def run_test(dut):
     pass
 
+
 main()
 
 import sys
-print(sys.argv[1])
-sys.exit(1)
+import os
+import pathlib
+
+
+def add_two_integers(a: int, b: int) -> int:
+    return a + b
+
+
+print(add_two_integers(3, 5))
+print(add_two_integers(1, 2))
+print(add_two_integers(2, 4))
+
+
+verilator_root = os.environ.get("VERILATOR_ROOT")
+if verilator_root is None:
+    raise EnvironmentError("VERILATOR_ROOT environment variable is not set.")
+
+
+verilator = pathlib.Path(verilator_root) / "bin" / "verilator"
+sys.path.insert(0, str(verilator))
+print(sys.path)
